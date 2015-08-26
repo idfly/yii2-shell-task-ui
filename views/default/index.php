dec6282a22c1467bb54d9bf044fcf92a316a60ad
Alexey Baranov <baranov.alxy@gmail.com>
2015-08-26 20:35:59 +0600

    #3463: list stub

--- views/default/index.php
+++ views/default/index.php
@@ -1,1 +1,29 @@
-<?php var_dump($tasks) ?>
+<?php
+use yii\helpers\Html;
+?>
+
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th></th>
+            <th>Название</th>
+            <th>Описание</th>
+            <th>Команда</th>
+            <th>Статус</th>
+            <th>Действия</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php foreach($tasks as $index => $task) : ?>
+            <tr>
+                <th scope="row">#<?= ++$index ?></th>
+                <td><?= Html::encode($task['name']) ?></th>
+                <td><?= Html::encode($task['description']) ?></td>
+                <td><?= Html::encode($task['cmd']) ?></td>
+                <td>todo</td>
+                <td>todo</td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
